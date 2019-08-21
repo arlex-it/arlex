d3499690432582106a193b307bbc0e2d271beefa
Lopacinski <augustin.lopacinski@epitech.eu>
2019-08-21 15:00:28 +0200

    poc_assist_vocale fix

--- Tasker/API/v1/user/PostCreate.py
+++ Tasker/API/v1/user/PostCreate.py
@@ -25,23 +25,19 @@ class APIV1UserPostCreate(AbstractView):
     """
     API to create a new user.
     """
-    # #@require_authentication('private')
-    # def __init__(self):
-    #     self.user_ctrlr = UserController()
-    #
-    # @expects_json(SCHEMA)
-    # def dispatch_request(self, **kwargs):
-    #     user = self.user_ctrlr.create(
-    #         firstname=g.data['firstname'],
-    #         lastname=g.data['lastname'],
-    #         phone=g.data['phone'],
-    #         email=g.data['email'],
-    #         password=g.data['password'],
-    #     )
-    #
-    #     response = HTTPResponse(200).set_content(user.id)
-    #     return response.get_response()
+    #@require_authentication('private')
+    def __init__(self):
+        self.user_ctrlr = UserController()
 
-    #@current_app.assist.action('Greetings')
-    def user_create(self):
-        return ask(speech='Welcome to {{ cookiecutter.project_name }}!')
+    @expects_json(SCHEMA)
+    def dispatch_request(self, **kwargs):
+        user = self.user_ctrlr.create(
+            firstname=g.data['firstname'],
+            lastname=g.data['lastname'],
+            phone=g.data['phone'],
+            email=g.data['email'],
+            password=g.data['password'],
+        )
+
+        response = HTTPResponse(200).set_content(user.id)
+        return response.get_response()
