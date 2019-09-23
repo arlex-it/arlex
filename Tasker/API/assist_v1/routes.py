3fc94229bba8207e82fb371f6674f5fb5e06b864
augustin.lopacinski@epitech.eu <augustin.lopacinski@epitech.eu@epitech.eu>
2019-09-23 18:33:11 +0200

    add alexa

--- Tasker/API/assist_v1/routes.py
+++ Tasker/API/assist_v1/routes.py
@@ -1,11 +1,16 @@
 import logging
+import json
+import time
 
 from flask import Blueprint
-from flask_assistant import Assistant, ask, tell
+from flask_ask import Ask, statement, question, session
+from flask_assistant import Assistant, tell
 from Tasker.controllers.ProductController import ProductController
 
 assit_blueprint_v1 = Blueprint('assist', __name__, url_prefix='/assist')
+ask_blueprint_v1 = Blueprint('alexa', __name__, url_prefix='/alexa')
 assist = Assistant(blueprint=assit_blueprint_v1)
+alexa = Ask(blueprint=ask_blueprint_v1)
 
 logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
 
@@ -14,3 +19,15 @@ def google_objets_count(productCount):
     product_ctrl = ProductController()
     nbrProduct = product_ctrl.CountNbrOfProduct()
     return tell(f'{nbrProduct} produits')
+
+@alexa.launch
+def start_skill():
+    return question("Oui ?")
+
+@alexa.intent('productCount')
+def alexa_product_count():
+    product_ctrl = ProductController()
+    nbrProduct = product_ctrl.CountNbrOfProduct()
+    # headlines = get_headlines()
+    # headlines_msg= 'The current world news headliens are {}'.format(headlines)
+    return statement(f'{nbrProduct} produits')
--- Tasker/API/core/routes.py
+++ Tasker/API/core/routes.py
@@ -7,7 +7,7 @@ from Tasker.API.core.HTTPReponse import HTTPResponse
 from Tasker.helpers.generic import load_config
 from Tasker.API.common.converters import UserConverter, ProductConverter
 from Tasker.API.v1.routes import blueprint_v1
-from Tasker.API.assist_v1.routes import assit_blueprint_v1
+from Tasker.API.assist_v1.routes import assit_blueprint_v1, ask_blueprint_v1
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -22,4 +22,5 @@ def register_routes(app):
     app.url_map.converters['user'] = UserConverter
     app.register_blueprint(blueprint_v1)
     app.register_blueprint(assit_blueprint_v1)
+    app.register_blueprint(ask_blueprint_v1)
 
