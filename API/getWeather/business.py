8d0f0a95b95e19c334bb119cd0eb87542d854fc9
Simon <bauchet.simon@gmail.com>
2019-10-12 19:21:49 +0100

    [training] add some changes travis

--- API/getWeather/business.py
+++ API/getWeather/business.py
@@ -5,17 +5,17 @@ import json
 
 
 def get_weather(request):
-    if not request:
+    if not request or request.json.get('city') is None:
         abort(400)
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+request.json.get('city')+'&APPID=c14ca5995bc9dbb8ef3f7b0abbb3be6e&units=metric')
+    city = request.json.get('city')
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=c14ca5995bc9dbb8ef3f7b0abbb3be6e&units=metric')
     res = json.loads(r.text)
-    print(res["cod"])
-    if int(res["cod"]) == 404:
-        abort(404, "Can't find the city")
-        return
-    j = {
-        'main_weather': res['weather'][0]['main'],
-        'description': res['weather'][0]['description'],
-        'temperature': res['main']['temp']
-    }
-    return jsonify(j)
+    if int(res["cod"]) == 200:
+        j = {
+            'main_weather': res['weather'][0]['main'],
+            'description': res['weather'][0]['description'],
+            'temperature': res['main']['temp']
+        }
+        return jsonify(j)
+    else:
+        abort(int(res["cod"]))
--- auto_test.py
+++ auto_test.py
@@ -1,27 +1,44 @@
 import requests
-import json
 
 get_weather_url = "http://127.0.0.1:5000/api/getweather/"
+get_weather_errors = 0
 
 
 def get_weather_request(param):
 	r = requests.post(get_weather_url, {}, param)
-	get_weather_check(r)
+	print("param: ", param, "code: ", r.status_code)
+	get_weather_check(r, param)
 
 
-def get_weather_check(r):
-	status_code = [200, 404]
-	if r.status_code not in status_code:
-		exit(1)
+def get_weather_check(r, param):
+	global get_weather_errors
+	error_code = [500]
+	if r.status_code in error_code:
+		print("ERROR GET_WEATHER: param: ", param)
+		get_weather_errors += 1
 
 
 def get_weather_process():
+	print("----- BEGIN GET_WEATHER -----")
 	get_weather_request({"city": "London"})
 	get_weather_request({"city": "dshqj"})
 	get_weather_request({"city": "167"})
 	get_weather_request({"city": "167ds:;d"})
-	get_weather_request({})
+	get_weather_request({"city": ""})
+	get_weather_request({"city": 123})
+	get_weather_request({"dkso": ""})
+	print(get_weather_errors, "ERROR(S)")
+	print("----- END GET_WEATHER -----")
+	return get_weather_errors
+
+
+def launcher():
+	errors = 0
+	errors += get_weather_process()
+	if errors != 0:
+		exit(1)
 
 
 if __name__ == '__main__':
-	get_weather_process()
+	launcher()
+
