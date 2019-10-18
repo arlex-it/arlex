1645848ba2363ba59ca1e1e3ad4e465d2df2e14e
Simon <bauchet.simon@gmail.com>
2019-10-18 10:18:20 +0100

    tmp

--- API/GetWeather/models.py
+++ API/GetWeather/models.py
@@ -1,6 +1,6 @@
 from Ressources.swagger_api import api
 from flask_restplus import fields
-
+#test
 getweather_input = api.model('getweather_input', {
     'city': fields.String(example='London', description='City name'),
 })
