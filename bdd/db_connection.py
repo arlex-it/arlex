2ae45bae664672d3dd9ceab67f2423cebf5e1599
Corentin <corentin.minne@epitech.eu>
2019-11-08 14:37:05 +0100

    Fixed eror on checking connection

--- bdd/db_connection.py
+++ bdd/db_connection.py
@@ -1,7 +1,5 @@
 #!env/bin/python
 
-import mysql.connector
-from mysql.connector import errorcode
 import sqlalchemy as db
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
@@ -16,7 +14,7 @@ def to_dict(model_instance, query_instance=None):
         return {c.name: str(getattr(model_instance, c.name)) for c in model_instance.__table__.columns}
     else:
         cols = query_instance.column_descriptions
-        return {cols[i]['name']: model_instance[i]  for i in range(len(cols)) }
+        return {cols[i]['name']: model_instance[i]  for i in range(len(cols))}
 
 
 class User(Base):
@@ -40,51 +38,13 @@ class User(Base):
     postal_code = Column(String(45), nullable=False)
 
 
-
-
 engine = db.create_engine('mysql+pymysql://corentin_local:corentinlocal@127.0.0.1/Arlex', pool_recycle=3600, echo=True)
-#engine.echo = True
-#metadata = db.MetaData(engine)
-#game = db.Table('user', metadata)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 try:
-    session.query("1").from_statement("SELECT 1").all()
+    session.query("1").all()
     print("\033[92mConnected to the Database !\033[0m")
-except:
-    print("\033[91mError with Database!\033[0m")
+except Exception as e:
+    print("\033[91mError with Database!\033[0m\n", e.args)
     exit(1)
-
-""",
-                Column('id', Integer, primary_key=True),
-                Column('trait_p', Integer),
-                Column('trait_e', Integer),
-                Column('trait_t', Integer),
-                Column('sante_p', Integer),
-                Column('sante_e', Integer),
-                Column('sante_t', Integer),
-                Column('mdv', Integer),
-                """
-
-
-"""try:
-    conn = mysql.connector.connect(host="127.0.0.1", user="corentin_local", password="corentinlocal",
-                                   database="Arlex")
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
-else:
-    cursor = conn.cursor(dictionary=True)
-    print("\033[92mConnected to the Database !\033[0m")
-"""
-
-#cursor.execute("""SELECT * FROM user;""")
-#rows = cursor.fetchall()
-#for row in cursor:
-#    print(row['id'], row['lastname'], row['mail'])
-#conn.close()
