710f7e129d1a461ab225ea0e8574672386955575
Simon <bauchet.simon@gmail.com>
2020-01-12 22:47:53 +0000

    facto unit_test code + add test

--- unit_test/user/tests/tests_post.py
+++ unit_test/user/tests/tests_post.py
@@ -1,24 +1,16 @@
 import unittest
 import datetime
 import requests
+from unit_test.user.user_model import get_user_model
 
 
 class UserRoute(unittest.TestCase):
     def testPostRoute(self, public_url=None):
-        new_user = {'date_insert': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                    'date_update': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                    'is_active': 0,
-                    'status': 0,
-                    'gender': 0,
-                    'lastname': 'Doe',
-                    'firstname': 'John',
-                    'mail': 'john@doe.com',
-                    'password': '$2y$12$Egy/Ye1Ikuy4oueV9ja7T.o3eDUvGFGO4ZgdQ2VSnbjvFOz29d7zK',
-                    'country': 'France',
-                    'town': 'Lille',
-                    'street': 'rue nationale',
-                    'street_number': '13',
-                    'region': 'Hauts de france',
-                    'postal_code': '59000'}
+        # create user
+        new_user = get_user_model()
         resp = requests.post(public_url + '/api/user'.format(), json=new_user)
         self.assertEqual(201, resp.status_code)
+
+        # try to create user with existing email
+        resp = requests.post(public_url + '/api/user'.format(), json=new_user)
+        self.assertEqual(403, resp.status_code)
