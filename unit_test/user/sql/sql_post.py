b44794b75f7d921e78f12d80d2f14f89898c0a5f
Simon <bauchet.simon@gmail.com>
2020-01-18 16:04:07 +0000

    add unit_test user_delete

--- unit_test/user/sql/sql_post.py
+++ unit_test/user/sql/sql_post.py
@@ -11,6 +11,11 @@ class PostSql:
         user = user_model_to_sql(user)
         self.session.add(user)
         self.session.commit()
+        return user.id
+
+    def get_user_by_id(self, id_user):
+        with self.engine.connect() as con:
+            rs = con.execute("SELECT * FROM user WHERE user.id = " + str(id_user))
 
     def delete_user_by_id(self, id_user):
         with self.engine.connect() as con:
