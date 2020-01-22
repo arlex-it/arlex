4e65005bbdd8e5cb99b387996195c8c2725221af
augustin.lopacinski@epitech.eu <augustin.lopacinski@epitech.eu@epitech.eu>
2020-01-22 16:18:45 +0100

    fix users

--- API/User/business.py
+++ API/User/business.py
@@ -116,10 +116,6 @@ def update_user(request, user_id):
         abort(400)
 
     infos = request.json
-    if not CheckAuthToken().check_user(request=request, id_user=user_id):
-        return HttpResponse(403).error(ErrorCode.BAD_TOKEN)
-    infos.pop('access_token')
-    infos.pop('refresh_token')
 
     """
     if user_connected_with_token != user_id:
