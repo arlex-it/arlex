2311adec4e868a80b2fa79b713c4894c2fbfa4f1
Corentin <corentin.minne@epitech.eu>
2020-01-22 15:57:38 +0100

    Add DELETE unit_test

--- unit_test/product/tests/tests_delete.py
+++ unit_test/product/tests/tests_delete.py
@@ -7,7 +7,7 @@ import requests
 import time
 
 
-class ProductsRouteGet(unittest.TestCase):
+class ProductsRouteDelete(unittest.TestCase):
 
     unit_test_init = UnitTestInit()
     engine, session = unit_test_init.connect_to_db()
@@ -17,21 +17,19 @@ class ProductsRouteGet(unittest.TestCase):
     def tearDown(self):
         self.sql.delete_all_product()
 
-    def test_get_product(self):
-        print(">>> test_get_product")
+    def test_delete_product(self):
+        print(">>> test_delete_product")
         new_product = get_product_model()
         product_id = self.sql.create_product(product=new_product)
-        resp = requests.get(self.public_url + '/api/products/{}'.format(product_id))
-        self.assertEqual(resp.status_code, 200)
+        resp = requests.delete(self.public_url + '/api/products/{}'.format(product_id))
+        self.assertEqual(resp.status_code, 202)
         product = self.sql.get_product_by_id(product_id)
-        self.assertEqual(product['expiration_date'].strftime("%Y-%m-%d"), new_product['expiration_date'])
-        self.assertEqual(product['id_ean'], new_product['id_ean'])
-        self.assertEqual(product['position'], new_product['position'])
+        self.assertEqual(product, None)
         time.sleep(0.5)
 
-    def test_get_wrong_product(self):
-        print(">>> test_get_wrong_product")
-        resp = requests.get(self.public_url + '/api/products/{}'.format(1000))
+    def test_delete_wrong_product(self):
+        print(">>> test_delete_wrong_product")
+        resp = requests.delete(self.public_url + '/api/products/{}'.format(1000))
         self.assertEqual(resp.status_code, 403)
         time.sleep(0.5)
 
