5379ab7580984b40552110bafedb21523b4c89c9
Simon <simon.bauchet@epitech.eu>
2020-04-29 11:52:17 +0200

    change useless concat

--- unit_test/product/tests/tests_post.py
+++ unit_test/product/tests/tests_post.py
@@ -13,7 +13,7 @@ class ProductRoutePost(unittest.TestCase):
     sql = PostSql(engine=engine, session=session)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    public_url = "http://" + "localhost" + ":5000"
+    public_url = "http://localhost:5000"
     s.close()
 
     def tearDown(self):
--- unit_test/user/tests/tests_delete.py
+++ unit_test/user/tests/tests_delete.py
@@ -14,7 +14,7 @@ class UserRouteDelete(unittest.TestCase):
     oauth_sql = UtilityOauthSQL(engine=engine, session= session)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    public_url = "http://" + "localhost" + ":5000"
+    public_url = "http://localhost:5000"
     s.close()
 
     def tearDown(self):
