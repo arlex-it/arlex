af38a3d5657917f0d3c7eecbf1f094842a28def7
Corentin Minne <corentin.minne@epitech.eu>
2020-05-14 16:02:28 +0200

    Voila Simon :)

--- API/Utilities/EanUtilities.py
+++ API/Utilities/EanUtilities.py
@@ -12,20 +12,21 @@ class EanUtilities:
 
     def search_product(self, product_list, asked):
         ean_list = []
+        best = ["", "", "", 0]
         for product in product_list:
             similarity = self.calc_similarity(product.product_name.lower(), asked.lower())
             similarity_gen = self.calc_similarity(product.product_name_gen.lower(), asked.lower())
             similarity = similarity_gen if similarity_gen > similarity else similarity
-            ean_list.append([product.product_name, product.product_name_gen, product.id_ean, similarity])
-        best = ean_list[0]
-        for ean in ean_list:
+            ean = [product.product_name, product.product_name_gen, product.id_ean, similarity]
             if ean[3] > best[3]:
                 best = ean
             print(ean)
+            if ean[3] >= 0.25:
+                ean_list.append([product.product_name, product.product_name_gen, product.id_ean, similarity])
         # print("\n\n\nle plus gros est : ", best)
         almost = []
         for ean in ean_list:
-            if ean != best and ean[3] >= 0.25 and ean[3] >= best[3] - best[3]/5:
+            if ean != best and ean[3] >= best[3] - best[3]/5:
                 almost.append(ean)
         # print("Il y a ", almost, "\nqui est peut etre bon egalement")
         ean_list = [best] + almost
