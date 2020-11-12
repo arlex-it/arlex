4eae5b734ba4d1deef70ad2c6a467dfe3a4219e6
Simon <bauchet.simon@gmail.com>
2020-11-12 10:47:50 +0100

    finish code documentation

--- API/OpenRequestCache/models.py
+++ API/OpenRequestCache/models.py
@@ -1,6 +1,5 @@
 from Ressources.swagger_api import api
 from flask_restplus import fields
 
-open_request_cache_input = api.model('open_request_cache_input', {
-    'url': fields.String(example='https://fr.openfoodfacts.org/produit/3222473310661', description='Url openfoodfact', required=True),
-})
+open_request_cache_input = api.parser()
+open_request_cache_input.add_arguement('url', location='path')
--- API/auth/models.py
+++ API/auth/models.py
@@ -8,10 +8,9 @@ auth_authorize_param.add_argument('state', location='path')
 auth_authorize_param.add_argument('user_locale', location='path')
 auth_authorize_param.add_argument('client_id', location='path')
 
-post_auth_model = api.model('Post auth', {
-    'client_id': fields.Integer(example=0, required=True),
-    'response_type': fields.String(example='code', description='Type de réponse', required=True),
-    'redirect_uri': fields.String(example='https://redirect_url', required=True),
-    'username': fields.String(example='john@doe.com', description='Adresse mail de l\'utilisateur', required=True),
-    'password': fields.String(example='password', description='Mot de passe de l\'utilisateur', required=True),
-})
+post_auth_model = api.parser()
+post_auth_model.add_argument('client_id', location='path')
+post_auth_model.add_argument('response_type', location='path')
+post_auth_model.add_argument('redirect_uri', location='path')
+post_auth_model.add_argument('username', location='path')
+post_auth_model.add_argument('password', location='path')
