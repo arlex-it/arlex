b4dc63749daa92ff9df37ed1bf637a0f0bd16dbc
Simon <bauchet.simon@gmail.com>
2020-11-13 15:08:46 +0100

    fix doc open_request_cache

--- API/OpenRequestCache/models.py
+++ API/OpenRequestCache/models.py
@@ -2,4 +2,4 @@ from Ressources.swagger_api import api
 from flask_restplus import fields
 
 open_request_cache_input = api.parser()
-open_request_cache_input.add_arguement('url', location='path')
+open_request_cache_input.add_argument('url', location='path')
